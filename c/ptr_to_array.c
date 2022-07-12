fc331e8bd6ab8b2a542e6090da2ff90f5ee2a44f
krish <krishnaveni.k@thundersoft.com>
2022-07-12 21:08:04 +0530

    12/07

--- c/ptr_to_array.c
+++ c/ptr_to_array.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 int main()
 {
+	/*1D Array
 	int i=0;
 	int a[5]={1,2,3,4,5};
 	int n=sizeof(a)/sizeof(a[0]);
@@ -9,14 +10,17 @@ int main()
 	
 	for(i=0;i<n;i++)
 		printf("%d\n",*(p+i));
+	*/
 		
-	int i=0;
-	int a[5]={1,2,3,4,5};
+	int i=0,j=0;
+	int a[2][2]={1,2,3,4};
 	int n=sizeof(a)/sizeof(a[0]);
-	int (*p)[5];
-	p=&a;
+	int (*p)[2];
+	p=a;
 	for(i=0;i<n;i++)
-		printf("%d\n",*(p+i));
-
+	{
+		for(j=0;j<n;j++)
+			printf("%d\n",*(*(p+i)+j));
+	}
 }
 	
