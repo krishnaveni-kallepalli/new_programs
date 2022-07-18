c86706a3e32bef38b64f0749ad479d9486de6465
krish <krishnaveni.k@thundersoft.com>
2022-07-18 17:19:08 +0530

    18/07

--- ds/sorting_bubble.c
+++ ds/sorting_bubble.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
-void insertion(int a[],int i,int j,int n)
+void bubble(int a[],int n)
 {
-	int temp;
-	for(i=1;i<n;i++)
+	int temp,i,j;
+	for(i=0;i<n;i++)
 	{
-		temp=a[i];
-		j=i-1;
-		while(j>=0 && a[j]>temp)
+		for(j=0;j<n-i-1;j++)
 		{
-			a[j+1]=a[j];
-			j--;
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp;
+				
+			}
 		}
-		a[j+1]=temp;
 	}
 }
 int main()
 {
 	int i,j,a[5],n;
-	void (*fp)(int a[],int i,int j,int n)=insertion;
+	void (*fp)(int a[],int n)=bubble;
 	n=sizeof(a)/sizeof(a[0]);
 	printf("Enter %d elements:\n",n);
 	for(i=0;i<n;i++)
 		scanf("%d",&a[i]);
-	fp(a,i,j,n);
+	fp(a,n);
 	printf("After sorting:\n");
 	for(i=0;i<n;i++)
 		printf("%d\n",a[i]);
